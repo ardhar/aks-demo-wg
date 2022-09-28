20e901157566a8e8a256f0e3a0108654d8aac6f7
Arindam Dhar <arindam_dhar@hotmail.com>
2022-09-28 21:00:34 +0530

    add new

--- Day-1/app.py
+++ Day-1/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
   
 @app.route('/')
 def hello():
-    return "<h1> WELCOME TO AKS WORKSHOP ! "
+    return "<h1> WELCOME TO AKS WORKSHOP - WALGREENS ! "
   
   
 if __name__ == "__main__":
